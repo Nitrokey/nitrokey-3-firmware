56a71434944f067b571dc649435dd83729bfd3d2
Jan Nordholz <j.nordholz@tu-berlin.de>
2022-02-17 02:41:56 +0100

    Start a clean-slate embedded runner project.
    The envisioned structure of the new firmware is:
    [app-nrf, app-lpc]
    -> SoC-specific RTIC App crates
    [librunner]
    -> generic library (initialization, glue towards Trussed etc.)
    [librunner::soc_*]
    -> SoC-specific types and defines for generic components

---
+++ runners/embedded/librunner/src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+
+#[macro_use]
+extern crate delog;
+delog::generate_macros!();
+
+mod types;
+
+#[cfg(feature = "soc-nrf52840")]
+pub mod soc_nrf52840;
+#[cfg(feature = "soc-nrf52840")]
+pub use soc_nrf52840 as soc;
+
+#[cfg(feature = "soc-lpc55")]
+pub mod soc_lpc55;
+#[cfg(feature = "soc-lpc55")]
+pub use soc_lpc55 as soc;
+
+#[cfg(not(any(feature = "soc-lpc55", feature = "soc-nrf52840")))]
+compile_error!("No SoC / board chosen!");
