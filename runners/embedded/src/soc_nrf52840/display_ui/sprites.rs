367410f20abf5faf1cc5cbdce09f0f2f38f482b5
Jan Nordholz <j.nordholz@tu-berlin.de>
2022-04-04 13:55:40 +0200

    Implement new additions to Trussed UI trait. (Bump dep in Cargo.lock.)

--- runners/embedded/src/soc_nrf52840/display_ui/sprites.rs
+++ runners/embedded/src/soc_nrf52840/display_ui/sprites.rs
@@ -1,17 +1,17 @@
 pub struct SpriteMap {
 	count_x: u16,
 	count_y: u16,
-	width: u16,
-	height: u16,
+	pub width: u16,
+	pub height: u16,
 	/* assume: bitsperpixel = 16 */
 	buf: &'static [u8],
 }
 
 const FONT: &[u8; (9*18*2)*192] = include_bytes!("../../../data/font_9x18.raw");
-const FONT_MAP: SpriteMap = SpriteMap { count_x: 1, count_y: 192, width: 9, height: 18, buf: FONT };
+pub const FONT_MAP: SpriteMap = SpriteMap { count_x: 1, count_y: 192, width: 9, height: 18, buf: FONT };
 
 const BATTERY: &[u8; (25*10*2)*6] = include_bytes!("../../../data/texmap.raw");
-const BATTERY_MAP: SpriteMap = SpriteMap { count_x: 1, count_y: 6, width: 25, height: 10, buf: BATTERY };
+pub const BATTERY_MAP: SpriteMap = SpriteMap { count_x: 1, count_y: 6, width: 25, height: 10, buf: BATTERY };
 
 pub enum SpriteErr {
 	UnknownError
@@ -19,8 +19,6 @@ pub enum SpriteErr {
 
 impl SpriteMap {
 	pub fn draw(&self, index: u16, dbuf: &mut [u8], dstride: u16) -> Result<(), SpriteErr> {
-		use core::convert::TryInto;
-
 		if index > self.count_x*self.count_y {
 			return Err(SpriteErr::UnknownError);
 		}
@@ -50,12 +48,6 @@ impl SpriteMap {
 		disp.blit_pixels(px, py, self.width, self.height, &tmpbuf[0..bufsz_needed]).map_err(|_| SpriteErr::UnknownError)
 	}
 }
-
-macro_rules! draw_sprite {
-($dsp:expr, $map:ident, $idx:expr, $px:expr, $py:expr) => {
-	$map.draw($idx, $dsp.buf, 0).ok();
-	$dsp.dsp.as_mut().unwrap().blit_at(&$dsp.buf[0..($map.width*$map.height*2) as usize], $px, $py, $map.width, $map.height);
-}}
 
 ////////////////////////////////////////////////////////////////////////////////
 
