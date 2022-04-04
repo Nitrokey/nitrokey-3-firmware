1b146c4d9b95f919ff471e2b30ef799ccc462610
Jan Nordholz <j.nordholz@tu-berlin.de>
2022-04-04 13:55:40 +0200

    Implement new additions to Trussed UI trait.

--- runners/embedded/src/soc_nrf52840/display_ui.rs
+++ runners/embedded/src/soc_nrf52840/display_ui.rs
@@ -1,23 +1,15 @@
-use embedded_graphics::{
-	// DrawTarget,
-	pixelcolor::{RgbColor, Rgb565, raw::RawU16, raw::RawData},
-};
 use embedded_hal::blocking::delay::DelayUs;
 use nrf52840_hal::{
-	gpio::{Pin, Input, Output, PullUp, PushPull, Floating, Level},
+	gpio::{Pin, Input, Output, PullUp, PushPull, Level},
 	pac::SPIM0,
-	prelude::{OutputPin, InputPin},
+	prelude::OutputPin,
 	spim::Spim,
-};
-use trussed::{
-	platform::{consent, reboot, ui},
 };
 
 mod sprites;
 
 type OutPin = Pin<Output<PushPull>>;
 type InPin = Pin<Input<PullUp>>;
-type FloatingPin = Pin<Input<Floating>>;
 
 type LLDisplay = picolcd114::ST7789<display_interface_spi::SPIInterfaceNoCS<Spim<SPIM0>, OutPin>, OutPin>;
 
@@ -67,50 +59,17 @@ impl DisplayUI {
 		if let Some(ref mut d) = self.disp {
 			if let Some(ref mut p) = self.disp_power {
 				p.set_low().ok();
+				delay_timer.delay_us(1000u32);
 			}
-			d.init(delay_timer);
+			d.init(delay_timer).ok();
 		}
 	}
 
 	pub fn power_off(&mut self) {
 		if let Some(ref mut p) = self.disp_power {
-			p.set_low().ok();
+			p.set_high().ok();
 		}
 		self.state = StickUIState::PoweredDown;
-	}
-
-	fn rgb16_memset(&mut self, color: Rgb565) {
-		// holy cow, Rust type inference/annotation is so sh*tty...
-		let c: u16 = Into::<RawU16>::into(color).into_inner();
-		let ch: u8 = (c >> 8) as u8;
-		let cl: u8 = (c & 255) as u8;
-		let buflen: usize = self.buf.len();
-
-		// the code generated from this looks more complicated than necessary;
-		// one day, replace all this nonsense with a tasty call to __aeabi_memset4()
-		// or figure out the "proper" Rust incantation the compiler happens to grasp
-		for i in (0..buflen).step_by(2) {
-			self.buf[i+0] = cl;
-			self.buf[i+1] = ch;
-		}
-	}
-
-	pub fn refresh(&mut self, t: u32) {
-		if t & 7 == 0 {
-			self.set_led(0, if (t & 8) == 8 { Level::High } else { Level::Low });
-		}
-	}
-
-	fn tile_bg(&mut self) {
-		if let Some(ref mut d) = self.disp {
-			let tile_buf: &[u8] = &self.buf[0..60*15*2];
-			for i in 0..4*9 {
-				let x = (i & 0b000011) * 60;
-				let y = (i >> 2) * 15;
-				// d.blit_at(&self.buf[0..60*15*2], x*60, y*15, 60, 15);
-				d.blit_pixels(x, y, 60, 15, tile_buf);
-			}
-		}
 	}
 
 	pub fn set_led(&mut self, idx: usize, lvl: Level) {
@@ -122,6 +81,83 @@ impl DisplayUI {
 		}
 	}
 
+	fn draw_text_line(&mut self, x: u16, y: u16, line: &[u8]) {
+		let mut xi: u16 = x;
+
+		if let Some(ref mut d) = self.disp {
+			for c0 in line {
+				let mut c: u16 = *c0 as u16;
+				if c == 0x0d {
+					continue;
+				} else if c >= 0x20 && c < 0x80 {
+					c = c - 0x20;
+				} else if c >= 0xa0 {
+					c = c - 0x40;
+				} else {
+					c = 0x7f - 0x20;
+				}
+				sprites::FONT_MAP.blit_single(c, self.buf, d, xi, y).ok();
+				xi += sprites::FONT_MAP.width;
+			}
+		}
+	}
 }
 
-impl trussed::platform::UserInterface for DisplayUI {}
+fn rgb565_memset(buf: &mut [u8], col: u16) {
+	let ch: u8 = (col >> 8) as u8;
+	let cl: u8 = (col & 255) as u8;
+	let buflen: usize = buf.len();
+
+	// the code generated from this looks more complicated than necessary;
+	// one day, replace all this nonsense with a tasty call to __aeabi_memset4()
+	// or figure out the "proper" Rust incantation the compiler happens to grasp
+	for i in (0..buflen).step_by(2) {
+		buf[i+0] = cl;
+		buf[i+1] = ch;
+	}
+}
+
+impl trussed::platform::UserInterface for DisplayUI {
+	// fn check_user_presence(&mut self) -> consent::Level
+	// fn set_status(&mut self, status: ui::Status)
+	// fn status(&self) -> ui::Status
+	// fn refresh(&mut self)
+	// fn uptime(&mut self) -> core::time::Duration
+	// fn reboot(&mut self, to: reboot::To) -> !
+	// fn wink(&mut self, duration: core::time::Duration)
+
+	fn draw_filled_rect(&mut self, x: u16, y: u16, w: u16, h: u16, col: u16) {
+		if let Some(ref mut d) = self.disp {
+			rgb565_memset(self.buf, col);
+			let (xs, ys): (u16, u16) = if h > w { (15, 60) } else { (60, 15) };
+			for xi in (x..x+w-1).step_by(xs as usize) {
+				for yi in (y..y+h-1).step_by(ys as usize) {
+					let xd = core::cmp::min(xs, x+w-xi);
+					let yd = core::cmp::min(ys, y+h-yi);
+					d.blit_pixels(xi, yi, xd, yd, &self.buf[0..(xd*yd*2) as usize]).ok();
+				}
+			}
+		}
+	}
+
+	fn draw_text(&mut self, x: u16, y: u16, text: &[u8]) {
+		let mut yi: u16 = y;
+
+		for line in text.split(|c| *c == 0x0a_u8) {
+			self.draw_text_line(x, yi, line);
+			yi += sprites::FONT_MAP.height;
+		}
+	}
+
+	fn draw_sprite(&mut self, x: u16, y: u16, sprite_map: u16, index: u16) {
+		let smap = match sprite_map {
+			1 => { sprites::BATTERY_MAP }
+			_ => { return; }
+		};
+		if let Some(ref mut d) = self.disp {
+			smap.blit_single(index, self.buf, d, x, y).ok();
+		}
+	}
+
+	fn get_gui_dimension(&self) -> Option<(u16, u16)> { Some((240, 135)) }
+}
