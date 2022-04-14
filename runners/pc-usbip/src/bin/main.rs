c97442a3b704e6794ade0d877890f57a68258961
Robin Krahl <robin@nitrokey.com>
2022-04-14 20:07:08 +0200

    pc-usbip: Add multi-app support and admin app

--- runners/pc-usbip/src/bin/main.rs
+++ runners/pc-usbip/src/bin/main.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use clap::Parser;
 use clap_num::maybe_hex;
 
@@ -5,7 +8,7 @@ use interchange::Interchange;
 use usb_device::{bus::UsbBusAllocator, prelude::*};
 use usbip_device::UsbIpBus;
 
-use solo_usbip::platform::init_platform;
+use solo_usbip::platform::{Platform, init_platform};
 use fido_authenticator;
 use usbd_ctaphid::constants::MESSAGE_SIZE;
 
@@ -40,6 +43,37 @@ struct Args {
     pid: u16,
 }
 
+struct Reboot;
+
+impl admin_app::Reboot for Reboot {
+    fn reboot() -> ! {
+        unimplemented!();
+    }
+
+    fn reboot_to_firmware_update() -> ! {
+        unimplemented!();
+    }
+
+    fn reboot_to_firmware_update_destructive() -> ! {
+        unimplemented!();
+    }
+
+    fn locked() -> bool {
+        false
+    }
+}
+
+#[derive(Clone)]
+struct Syscall {
+    service: Rc<RefCell<trussed::service::Service<Platform>>>,
+}
+
+impl trussed::client::Syscall for Syscall {
+    fn syscall(&mut self) {
+        log::debug!("syscall");
+        self.service.borrow_mut().process();
+    }
+}
 
 fn main() {
     #[cfg(feature = "enable-logs")]
@@ -49,9 +83,7 @@ fn main() {
     log::info!("Initializing Trussed");
     let state_file = args.state_file;
     let trussed_platform = init_platform(state_file);
-    let mut trussed_service = trussed::service::Service::new(trussed_platform);
-    let client_id = "fido";
-    let trussed_client = trussed_service.try_as_new_client(client_id).unwrap();
+    let trussed_service = Rc::new(RefCell::new(trussed::service::Service::new(trussed_platform)));
 
     log::info!("Initializing allocator");
     // To change IP or port see usbip-device-0.1.4/src/handler.rs:26
@@ -70,15 +102,22 @@ fn main() {
         .device_sub_class(0)
         .build();
 
-    let fido_auth =
-        fido_authenticator::Authenticator::new(trussed_client, fido_authenticator::Conforming {},
-                                               fido_authenticator::Config { max_msg_size: MESSAGE_SIZE});
-    let mut fido_app = fido_auth;
+    let syscall = Syscall { service: trussed_service.clone() };
+
+    let trussed_client = trussed_service.borrow_mut().try_new_client("fido", syscall.clone()).unwrap();
+    let mut fido_app = fido_authenticator::Authenticator::new(
+        trussed_client,
+        fido_authenticator::Conforming {},
+        fido_authenticator::Config { max_msg_size: MESSAGE_SIZE},
+    );
+
+    let trussed_client = trussed_service.borrow_mut().try_new_client("admin", syscall.clone()).unwrap();
+    let mut admin_app = admin_app::App::<_, Reboot>::new(trussed_client, [0; 16], 0);
 
     log::info!("Ready for work");
     loop {
         std::thread::sleep(std::time::Duration::from_millis(5));
-        ctaphid_dispatch.poll(&mut [&mut fido_app]);
+        ctaphid_dispatch.poll(&mut [&mut fido_app, &mut admin_app]);
         usb_bus.poll(&mut [&mut ctaphid]);
     }
 }
