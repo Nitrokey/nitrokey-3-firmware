574105b8d63263572316308f305b66a5ef94d98b
Markus Meissner <coder@safemailbox.de>
2022-07-04 17:53:23 +0200

    move bootloader, fido-test-key gen to utils/

--- utils/gen-fido-keys/gen_init_fs.c
+++ utils/gen-fido-keys/gen_init_fs.c
@@ -5,63 +5,15 @@
 #include <fcntl.h>
 #include "lfs.h"
 
-#if !defined(FILESYSTEM_BASE) || !defined(FILESYSTEM_SIZE)
-# error Definitions for FILESYSTEM_BASE and FILESYSTEM_SIZE missing.
-#endif
-
 static void *fsmmap = NULL;
-
-static int mm_read(const struct lfs_config *c, lfs_block_t b, lfs_off_t o, void *buf, lfs_size_t sz) {
-	printf("F RD %02x+%04x %04x (%p)\n", b, o, sz, __builtin_return_address(1));
-	(void)c;
-	memcpy(buf, ((char *)fsmmap) + (b<<12) + o, sz);
-	return LFS_ERR_OK;
-}
-
-static int mm_prog(const struct lfs_config *c, lfs_block_t b, lfs_off_t o, const void *buf, lfs_size_t sz) {
-	printf("F WR %02x+%04x %04x (%p)\n", b, o, sz, __builtin_return_address(1));
-	(void)c;
-	memcpy(((char *)fsmmap) + (b<<12) + o, buf, sz);
-	return LFS_ERR_OK;
-}
-
-static int mm_erase(const struct lfs_config *c, lfs_block_t b) {
-	printf("F ER %02x\n", b);
-	memset(((char *)fsmmap) + (b<<12), 0xff, c->block_size);
-	return LFS_ERR_OK;
-}
-
-static int mm_noop(const struct lfs_config *c) {
-	(void)c;
-	return LFS_ERR_OK;
-}
-
-static struct lfs LFS;
-static struct lfs_config LFS_CONFIG = {
-	.context = NULL,
-	.read = mm_read,
-	.prog = mm_prog,
-	.erase = mm_erase,
-	.sync = mm_noop,
-#ifdef LFS_THREADSAFE
-	.lock = mm_noop,
-	.unlock = mm_noop,
-#endif
-	.read_size = 4,
-	.prog_size = 4,
-	.block_size = 0x1000,
-	.block_count = FILESYSTEM_SIZE / 0x1000,
-	.block_cycles = -1,
-	.cache_size = 256,
-	.lookahead_size = 32,
-	.read_buffer = NULL,
-	.prog_buffer = NULL,
-	.lookahead_buffer = NULL,
-	.name_max = 0,
-	.file_max = 0,
-	.attr_max = 0,
-	.metadata_max = 0
+static char rndbuf[32] = {
+	0,1,2,3,4,5,6,7,
+	0,1,2,3,4,5,6,7,
+	0,1,2,3,4,5,6,7,
+	0,1,2,3,4,5,6,7
 };
+
+#include "my_lfs_common.h"
 static struct lfs_file LFS_FILE;
 
 int main(int an, char **ac) {
@@ -115,6 +67,20 @@ int main(int an, char **ac) {
 		close(r2);
 
 		r = lfs_file_write(&LFS, &LFS_FILE, buffer, len);
+		printf("file write done, ret: %d\n", r);
+		r = lfs_file_close(&LFS, &LFS_FILE);
+		printf("file close done, ret: %d\n", r);
+	}
+
+	r = lfs_mkdir(&LFS, "/trussed");
+	printf("mkdir /trussed done, ret: %d\n", r);
+	r = lfs_mkdir(&LFS, "/trussed/dat");
+	printf("mkdir /trussed/dat done, ret: %d\n", r);
+	memset(&LFS_FILE, 0, sizeof(LFS_FILE));
+	r = lfs_file_open(&LFS, &LFS_FILE, "/trussed/dat/rng-state.bin", LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC);
+	printf("open /trussed/dat/rng-state.bin done, ret: %d\n", r);
+	if (r == 0) {
+		r = lfs_file_write(&LFS, &LFS_FILE, rndbuf, 32);
 		printf("file write done, ret: %d\n", r);
 		r = lfs_file_close(&LFS, &LFS_FILE);
 		printf("file close done, ret: %d\n", r);
