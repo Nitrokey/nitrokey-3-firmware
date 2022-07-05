ab246e51fb87e342a960a843b267ce088df5e11f
Markus Meissner <coder@safemailbox.de>
2022-07-05 23:38:57 +0200

    integrate #51 & #53 into embedded runner

--- runners/embedded/src/soc_lpc55/trussed.rs
+++ runners/embedded/src/soc_lpc55/trussed.rs
@@ -11,9 +11,11 @@ use crate::traits::{
 	buttons::{Press, Edge},
 	rgb_led::RgbLed
 };
+use crate::ui::Status;
 use trussed::platform::{
-    ui, reboot, consent,
+    reboot, consent,
 };
+
 
 // translated from https://stackoverflow.com/a/2284929/2490057
 fn sin(x: f32) -> f32
@@ -31,7 +33,7 @@ fn sin(x: f32) -> f32
     res
 }
 
-// Assuming there will only be one way to 
+// Assuming there will only be one way to
 // get user presence, this should be fine.
 // Used for Ctaphid.keepalive message status.
 static mut WAITING: bool = false;
@@ -54,7 +56,7 @@ RGB: RgbLed,
     rtc: Rtc<init_state::Enabled>,
     buttons: Option<BUTTONS>,
     rgb: Option<RGB>,
-    wink: Option<core::ops::Range<Duration>>,
+    status: Status,
     provisioner: bool,
 }
 
@@ -69,13 +71,23 @@ RGB: RgbLed,
         rgb: Option<RGB>,
         provisioner: bool,
     ) -> Self {
-        let wink = None;
-        #[cfg(not(feature = "no-buttons"))]
-        let ui = Self { rtc, buttons: _buttons, rgb, wink, provisioner };
-        #[cfg(feature = "no-buttons")]
-        let ui = Self { rtc, buttons: None, rgb, wink, provisioner };
+        let uptime = rtc.uptime();
+        let status = Status::Startup(uptime);
 
+        #[cfg(not(feature = "no-buttons"))]
+        let mut ui = Self { rtc, buttons: _buttons, status, rgb, provisioner };
+        #[cfg(feature = "no-buttons")]
+        let mut ui = Self { rtc, buttons: None, status, rgb, provisioner };
+
+        ui.refresh_ui(uptime);
         ui
+    }
+    fn refresh_ui(&mut self, uptime: Duration) {
+        if let Some(rgb) = &mut self.rgb {
+            self.status.refresh(uptime);
+            let mode = self.status.led_mode(self.provisioner);
+            rgb.set(mode.color(uptime));
+        }
     }
 }
 
@@ -112,84 +124,15 @@ RGB: RgbLed,
         }
     }
 
-    fn set_status(&mut self, status: ui::Status) {
-        if let Some(rgb) = &mut self.rgb {
-
-            match status {
-                ui::Status::Idle => {
-                    if self.provisioner {
-                        // white
-                        rgb.set(0xff_ff_ff.into());
-                    } else {
-                        // green
-                        rgb.set(0x00_ff_02.into());
-                    }
-                },
-                ui::Status::Processing => {
-                    // teal
-                    rgb.set(0x00_ff_5a.into());
-                }
-                ui::Status::WaitingForUserPresence => {
-                    // orange
-                    rgb.set(0xff_7e_00.into());
-                },
-                ui::Status::Error => {
-                    // Red
-                    rgb.set(0xff_00_00.into());
-                },
-            }
-
-        }
-
-        // Abort winking if the device is no longer idle
-        if status != ui::Status::Idle {
-            self.wink = None;
-        }
+    fn set_status(&mut self, status: trussed::platform::ui::Status) {
+        let uptime = self.uptime();
+        self.status.update(status, uptime);
+        self.refresh_ui(uptime);
     }
 
     fn refresh(&mut self) {
-        if self.rgb.is_none() {
-            return;
-        }
-
-        if let Some(wink) = self.wink.clone() {
-            let time = self.uptime();
-            if wink.contains(&time) {
-                // 250 ms white, 250 ms off
-                let color = if (time - wink.start).as_millis() % 500 < 250 {
-                    0xff_ff_ff
-                } else {
-                    0x00_00_00
-                };
-                self.rgb.as_mut().unwrap().set(color.into());
-                return;
-            } else {
-                self.wink = None;
-            }
-        }
-
-        if self.buttons.is_some() {
-            // 1. Get time & pick a period (here 4096).
-            // 2. Map it to a value between 0 and pi.
-            // 3. Calculate sine and map to amplitude between 0 and 255.
-            let time = (self.uptime().as_millis()) % 4096;
-            let amplitude = (sin((time as f32) * 3.14159265f32/4096f32) * 255f32) as u32;
-
-            let state = self.buttons.as_mut().unwrap().state();
-            let color = if state.a || state.b || state.middle {
-                // Use blue if button is pressed.
-                0x00_00_01 | (amplitude << 0)
-            } else {
-                // Use green if no button is pressed.
-                0x00_00_01 | (amplitude << 8)
-            };
-            // use logging::hex::*;
-            // use logging::hex;
-            // crate::logger::info!("time: {}", time).ok();
-            // crate::logger::info!("amp: {}", hex!(amplitude)).ok();
-            // crate::logger::info!("color: {}", hex!(color)).ok();
-            self.rgb.as_mut().unwrap().set(color.into());
-        }
+        let uptime = self.uptime();
+        self.refresh_ui(uptime);
     }
 
     fn uptime(&mut self) -> Duration {
@@ -202,8 +145,8 @@ RGB: RgbLed,
     }
 
     fn wink(&mut self, duration: Duration) {
-        let time = self.uptime();
-        self.wink = Some(time..time + duration);
-        self.rgb.as_mut().unwrap().set(0xff_ff_ff.into());
+        let uptime = self.uptime();
+        self.status = Status::Winking(uptime..uptime + duration);
+        self.refresh_ui(uptime);
     }
 }
