a4a0d5e1ef69356bbeab47d22c053caefc44cd1c
Sosthène Guédon <sosthene@nitrokey.com>
2022-11-15 15:59:41 +0100

    Increase heap size

--- runners/embedded/src/bin/app-nrf.rs
+++ runners/embedded/src/bin/app-nrf.rs
@@ -57,7 +57,7 @@ mod app {
     fn init(mut ctx: init::Context) -> (SharedResources, LocalResources, init::Monotonics) {
         {
             use core::mem::MaybeUninit;
-            const HEAP_SIZE: usize = 8024;
+            const HEAP_SIZE: usize = 24 * 1024;
             static mut HEAP: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
             unsafe { crate::ALLOCATOR.init(HEAP.as_ptr() as usize, HEAP_SIZE) }
         }
