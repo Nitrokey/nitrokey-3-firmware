97e0147e6587c2e369e8b130ae3eaf2a15c74e35
Sosthène Guédon <sosthene@nitrokey.com>
2023-03-07 16:35:52 +0100

    Move dispatch implementation to its own module

--- components/apps/src/lib.rs
+++ components/apps/src/lib.rs
@@ -6,28 +6,16 @@ use apdu_dispatch::{
 use core::marker::PhantomData;
 use ctaphid_dispatch::app::App as CtaphidApp;
 use trussed::{
-    api::{Reply, Request},
-    backend::BackendId,
-    client::ClientBuilder,
-    error::Error as TrussedError,
-    platform::Syscall,
-    service::ServiceResources,
-    types::{Context, Location},
-    ClientImplementation, Platform, Service,
+    backend::BackendId, client::ClientBuilder, platform::Syscall, ClientImplementation, Platform,
+    Service,
 };
-
-#[cfg(feature = "backend-auth")]
-use trussed::{
-    api::{reply, request},
-    backend::Backend as _,
-    serde_extensions::{ExtensionDispatch, ExtensionId, ExtensionImpl},
-    Bytes,
-};
-#[cfg(feature = "backend-auth")]
-use trussed_auth::{AuthBackend, AuthContext, AuthExtension, MAX_HW_KEY_LEN};
 
 #[cfg(feature = "admin-app")]
 pub use admin_app::Reboot;
+
+mod dispatch;
+use dispatch::Backend;
+pub use dispatch::Dispatch;
 
 pub trait Runner {
     type Syscall: Syscall;
@@ -65,132 +53,6 @@ type OpcardApp<R> = opcard::Card<Client<R>>;
 #[cfg(feature = "provisioner-app")]
 type ProvisionerApp<R> =
     provisioner_app::Provisioner<<R as Runner>::Store, <R as Runner>::Filesystem, Client<R>>;
-
-#[derive(Debug)]
-pub struct Dispatch {
-    #[cfg(feature = "backend-auth")]
-    auth: AuthBackend,
-}
-
-#[derive(Debug, Default)]
-pub struct DispatchContext {
-    #[cfg(feature = "backend-auth")]
-    auth: AuthContext,
-}
-
-impl Dispatch {
-    pub fn new(auth_location: Location) -> Self {
-        #[cfg(not(feature = "backend-auth"))]
-        let _ = auth_location;
-        Self {
-            #[cfg(feature = "backend-auth")]
-            auth: AuthBackend::new(auth_location),
-        }
-    }
-    #[cfg(feature = "backend-auth")]
-    pub fn with_hw_key(auth_location: Location, hw_key: Bytes<MAX_HW_KEY_LEN>) -> Self {
-        Self {
-            auth: AuthBackend::with_hw_key(auth_location, hw_key),
-        }
-    }
-}
-
-#[cfg(feature = "backend-auth")]
-impl ExtensionDispatch for Dispatch {
-    type Context = DispatchContext;
-    type BackendId = Backend;
-    type ExtensionId = Extension;
-
-    fn core_request<P: Platform>(
-        &mut self,
-        backend: &Self::BackendId,
-        ctx: &mut Context<Self::Context>,
-        request: &Request,
-        resources: &mut ServiceResources<P>,
-    ) -> Result<Reply, TrussedError> {
-        match backend {
-            Backend::Auth => {
-                self.auth
-                    .request(&mut ctx.core, &mut ctx.backends.auth, request, resources)
-            }
-        }
-    }
-
-    fn extension_request<P: Platform>(
-        &mut self,
-        backend: &Self::BackendId,
-        extension: &Self::ExtensionId,
-        ctx: &mut Context<Self::Context>,
-        request: &request::SerdeExtension,
-        resources: &mut ServiceResources<P>,
-    ) -> Result<reply::SerdeExtension, TrussedError> {
-        match backend {
-            Backend::Auth => match extension {
-                Extension::Auth => self.auth.extension_request_serialized(
-                    &mut ctx.core,
-                    &mut ctx.backends.auth,
-                    request,
-                    resources,
-                ),
-            },
-        }
-    }
-}
-
-#[cfg(not(feature = "backend-auth"))]
-impl trussed::backend::Dispatch for Dispatch {
-    type Context = DispatchContext;
-    type BackendId = Backend;
-    fn request<P: Platform>(
-        &mut self,
-        backend: &Self::BackendId,
-        _ctx: &mut Context<Self::Context>,
-        _request: &Request,
-        _resources: &mut ServiceResources<P>,
-    ) -> Result<Reply, TrussedError> {
-        match *backend {}
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Backend {
-    #[cfg(feature = "backend-auth")]
-    Auth,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Extension {
-    #[cfg(feature = "backend-auth")]
-    Auth,
-}
-
-impl From<Extension> for u8 {
-    fn from(extension: Extension) -> Self {
-        match extension {
-            #[cfg(feature = "backend-auth")]
-            Extension::Auth => 0,
-        }
-    }
-}
-
-impl TryFrom<u8> for Extension {
-    type Error = TrussedError;
-
-    fn try_from(id: u8) -> Result<Self, Self::Error> {
-        match id {
-            #[cfg(feature = "backend-auth")]
-            0 => Ok(Extension::Auth),
-            _ => Err(TrussedError::InternalError),
-        }
-    }
-}
-
-#[cfg(feature = "backend-auth")]
-impl ExtensionId<AuthExtension> for Dispatch {
-    type Id = Extension;
-
-    const ID: Self::Id = Self::Id::Auth;
-}
 
 pub struct Apps<R: Runner> {
     #[cfg(feature = "admin-app")]
