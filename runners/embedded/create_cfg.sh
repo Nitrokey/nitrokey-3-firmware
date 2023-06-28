3b17d6ab8d3ac39871febfaf0d700fd2faff0fa5
Sosthène Guédon <sosthene@nitrokey.com>
2023-06-28 09:37:35 +0200

    Don't call rm when the cfg file has been moved

--- runners/embedded/create_cfg.sh
+++ runners/embedded/create_cfg.sh
@@ -6,7 +6,7 @@ BOARD="$3"
 
 
 TMP_CFG=$(mktemp)
-cp -f "$CFG_PATH" $TMP_CFG
+cp "$CFG_PATH" $TMP_CFG
 echo '' >> $TMP_CFG
 echo '[build]' >> $TMP_CFG
 echo "build_profile = \"$BUILD_PROFILE\"" >> $TMP_CFG
@@ -15,5 +15,6 @@ echo "board = \"$BOARD\"" >> $TMP_CFG
 diff $TMP_CFG cfg.toml 
 if [ "$?" != 0 ]; then
   mv -f $TMP_CFG cfg.toml
+else
+  rm $TMP_CFG
 fi
-rm -f $TMP_CFG
