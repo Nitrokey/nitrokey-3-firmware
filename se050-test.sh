dd2882876ec37d88876191d3464e4543eb32b888
Sosthène Guédon <sosthene@nitrokey.com>
2023-07-21 13:59:28 +0200

    Ciphers and MACs work

--- se050-test.sh
+++ se050-test.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+!/bin/sh
 ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D1:00:00
-../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:84:00:00:00:0104 
-../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D4:00:00:00:0104
-../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D5:00:00:00:0104 
-../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D6:00:00
-../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D7:00:00:00
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:84:00:00:00:0104 
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D4:00:00:00:0104
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D5:00:00:00:0104 
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D6:00:00
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D7:00:00:00
 # ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D8:00:00:00
-../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:DA:00:00:00
- # ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D9:00:00:00
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:DA:00:00:00
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:D9:00:00:00
+# time ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:DB:00:00:00
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:DE:00:00:00
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:E0:00:00
+../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:DE:00:00:00
+# ../OpenSC/src/tools/opensc-tool -c default -s 00:A4:04:00:05:D2760001FF -s 00:DF:00:00:00
