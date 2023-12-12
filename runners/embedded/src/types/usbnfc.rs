a727dff15b9a739dc4962841983fae98b06f52d5
Sosthène Guédon <sosthene@nitrokey.com>
2023-12-12 14:57:43 +0100

    Remove serial usb driver
    Not used

--- runners/embedded/src/types/usbnfc.rs
+++ runners/embedded/src/types/usbnfc.rs
@@ -3,7 +3,6 @@ use nfc_device::Iso14443;
 use usb_device::device::UsbDevice;
 use usbd_ccid::Ccid;
 use usbd_ctaphid::CtapHid;
-use usbd_serial::SerialPort;
 
 use crate::types::{ApduDispatch, CtaphidDispatch, Soc};
 
@@ -11,15 +10,13 @@ pub struct UsbClasses<S: Soc> {
     pub usbd: UsbDevice<'static, S::UsbBus>,
     pub ccid: Ccid<'static, 'static, S::UsbBus, CCID_SIZE>,
     pub ctaphid: CtapHid<'static, 'static, 'static, S::UsbBus>,
-    pub serial: SerialPort<'static, S::UsbBus>,
 }
 
 impl<S: Soc> UsbClasses<S> {
     pub fn poll(&mut self) {
         self.ctaphid.check_for_app_response();
         self.ccid.check_for_app_response();
-        self.usbd
-            .poll(&mut [&mut self.ccid, &mut self.ctaphid, &mut self.serial]);
+        self.usbd.poll(&mut [&mut self.ccid, &mut self.ctaphid]);
     }
 }
 
