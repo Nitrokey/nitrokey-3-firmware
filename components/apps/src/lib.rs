c69914811815f02795c5575e0102389ad290ec61
Sosthène Guédon <sosthene@nitrokey.com>
2024-09-30 16:12:31 +0200

    PIV: use se050 backend and trussed-auth for key wrapping

--- components/apps/src/lib.rs
+++ components/apps/src/lib.rs
@@ -1055,8 +1055,7 @@ impl<R: Runner> App<R> for PivApp<R> {
     }
     fn backends(runner: &R, _: &()) -> &'static [BackendId<Backend>] {
         const BACKENDS_PIV: &[BackendId<Backend>] = &[
-            BackendId::Custom(Backend::SoftwareRsa),
-            BackendId::Custom(Backend::Auth),
+            BackendId::Custom(Backend::Se050),
             BackendId::Custom(Backend::Staging),
             BackendId::Core,
         ];
