2e32ba3b086dc7f59ed44dc2583cf16d1755a158
Robin Krahl <robin@nitrokey.com>
2026-03-13 12:31:39 +0100

    stm32n6: Use timer to blink LEDs

--- runners/nkso3/src/main.rs
+++ runners/nkso3/src/main.rs
@@ -2,35 +2,91 @@
 #![no_main]
 
 mod gpio;
+mod timer;
 
 use core::panic::PanicInfo;
 
 use cortex_m_rt::{entry, exception, ExceptionFrame};
 use cortex_m_semihosting::hprintln;
-use embedded_hal::digital::v2::OutputPin as _;
+use embedded_hal::digital::v2::{OutputPin as _, PinState};
 use stm32n6::stm32n657::Peripherals;
 
-use self::gpio::{GpioG, PinG0, PinG10, PinG8};
+use self::{
+    gpio::{GpioG, OutputMode, PinG0, PinG10, PinG8},
+    timer::{Tim6, Timer},
+};
 
-// user red LED (LD5): PG10
-// user green LED (LD6): PG0
-// user blue LED (LD7): PG8
+#[derive(Clone, Copy)]
+enum Led {
+    // user red LED (LD5): PG10
+    Ld5,
+    // user green LED (LD6): PG0
+    Ld6,
+    // user blue LED (LD7): PG8
+    Ld7,
+}
+
+impl Led {
+    fn next(self) -> Self {
+        match self {
+            Self::Ld5 => Self::Ld6,
+            Self::Ld6 => Self::Ld7,
+            Self::Ld7 => Self::Ld5,
+        }
+    }
+}
+
+struct Leds<'a> {
+    ld5: PinG10<'a, OutputMode>,
+    ld6: PinG0<'a, OutputMode>,
+    ld7: PinG8<'a, OutputMode>,
+}
+
+impl<'a> Leds<'a> {
+    fn new(gpiog: &'a GpioG) -> Self {
+        let ld5 = PinG10::new(gpiog).into_push_pull_output();
+        let ld6 = PinG0::new(gpiog).into_push_pull_output();
+        let ld7 = PinG8::new(gpiog).into_push_pull_output();
+        let mut leds = Self { ld5, ld6, ld7 };
+        for led in [Led::Ld5, Led::Ld6, Led::Ld7] {
+            leds.set(led, false);
+        }
+        leds
+    }
+
+    fn set(&mut self, led: Led, on: bool) {
+        let state = PinState::from(!on);
+        match led {
+            Led::Ld5 => self.ld5.set_state(state).ok(),
+            Led::Ld6 => self.ld6.set_state(state).ok(),
+            Led::Ld7 => self.ld7.set_state(state).ok(),
+        };
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    hprintln!("nkso3 firmrware is running").ok();
+    hprintln!("nkso3 firmware is running").ok();
 
     let p = Peripherals::take().unwrap();
     let gpiog = GpioG::new(p.GPIOG_S, &p.RCC);
+    let mut leds = Leds::new(&gpiog);
 
-    let mut ld5 = PinG10::new(&gpiog).into_push_pull_output();
-    let mut ld6 = PinG0::new(&gpiog).into_push_pull_output();
-    let mut ld7 = PinG8::new(&gpiog).into_push_pull_output();
-    ld5.set_low().ok();
-    ld6.set_low().ok();
-    ld7.set_low().ok();
+    let tim6 = Tim6::new(p.TIM6_S, &p.RCC);
+    let mut timer = Timer::new(tim6);
+    // values are based on an input clock of 4 MHz, so the timer clock would be
+    // 4 MHz / (9999 + 1) = 4 KHz, so update occurs with frequency 4 KHz / (3999 + 1) = 1Hz.
+    // but from observation I think the actual frequency is slightly higher
+    timer.start(9999, 3999);
 
-    loop {}
+    let mut led = Led::Ld5;
+    leds.set(led, true);
+    loop {
+        nb::block!(timer.wait()).ok();
+        leds.set(led, false);
+        led = led.next();
+        leds.set(led, true);
+    }
 }
 
 #[inline(never)]
