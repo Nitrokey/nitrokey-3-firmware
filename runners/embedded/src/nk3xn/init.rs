4e00cff1f5c9cf7d24edfc3f8e58e969cc6989c0
Sosthène Guédon <sosthene@nitrokey.com>
2026-09-10 11:17:28 +0200

    Fix unnecessary 100ms wait for I2C when over NFC

--- runners/embedded/src/nk3xn/init.rs
+++ runners/embedded/src/nk3xn/init.rs
@@ -604,10 +604,10 @@ impl Stage2 {
                 .unwrap()
                 .into_gpio_pin(&mut self.clocks.iocon, &mut self.clocks.gpio)
                 .into_output_high();
-        }
 
-        self.basic.delay_timer.start(100_000.microseconds());
-        nb::block!(self.basic.delay_timer.wait()).ok();
+            self.basic.delay_timer.start(100_000.microseconds());
+            nb::block!(self.basic.delay_timer.wait()).ok();
+        }
 
         let token = self.clocks.clocks.support_flexcomm_token().unwrap();
         let i2c = flexcomm5.enabled_as_i2c(&mut self.peripherals.syscon, &token);
