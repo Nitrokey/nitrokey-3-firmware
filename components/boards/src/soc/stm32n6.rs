6287dbdaaff50c98a12701432b8300245f8603e6
Sosthène Guédon <sosthene@nitrokey.com>
2026-09-24 17:39:11 +0200

    board stm32n6: add mmc typedef

--- components/boards/src/soc/stm32n6.rs
+++ components/boards/src/soc/stm32n6.rs
@@ -20,6 +20,36 @@ pub struct Stm32n6 {
     uuid: Uuid,
 }
 
+pub mod mmc {
+    use stm32n6::stm32n657::SDMMC2_S;
+    use stm32n657_hal::gpio::{
+        Alternate, PinC0, PinC2, PinC3, PinC4, PinC5, PinE4, PullUp, ALTERNATE_FUNCTION_11,
+    };
+    use stm32n657_hal::mmc::MmcMaster;
+    use stm32n657_hal::sdmmc::Enabled;
+
+    pub type CmdPin = PinC3<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>;
+    pub type CkPin = PinC2<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>;
+    pub type D0Pin = PinC4<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>;
+    #[allow(unused)]
+    pub type D1Pin = PinC5<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>;
+    #[allow(unused)]
+    pub type D2Pin = PinC0<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>;
+    #[allow(unused)]
+    pub type D3Pin = PinE4<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>;
+
+    pub type Pins = (
+        CmdPin,
+        CkPin,
+        D0Pin,
+        // PinC5<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>,
+        // PinC0<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>,
+        // PinE4<Alternate<PullUp, { ALTERNATE_FUNCTION_11 }>>,
+    );
+
+    pub type Mmc = MmcMaster<SDMMC2_S, Pins, Enabled>;
+}
+
 impl Soc for Stm32n6 {
     type UsbBus = UsbBus1;
     type Clock = TimerClock;
